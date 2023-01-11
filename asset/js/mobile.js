e6fc74f6bf2cb24ca716e3d33047c40a0c7908c3
adisak.j <adisak@ava.fund>
2023-01-11 11:53:03 +0700

    ffff

--- asset/js/mobile.js
+++ asset/js/mobile.js
@@ -1,43 +1,6 @@
-function initialData(data) {
-  console.log("############ initialData ############");
-}
- 
 //##################################################################
 // Example interfaces between your JavaScript code and client-side Android / iOS code
 //##################################################################
-
-//Example for call native by pass parameter as string
-//message: String
-function example1(message) {
-  // Call iOS
-  if (window.webkit != undefined) {
-    if (window.webkit.messageHandlers.appInterface != undefined) {
-      window.webkit.messageHandlers.example1.postMessage(message);
-    }
-  }
-
-  // Call Android
-  if (typeof App !== "undefined") {
-    App.example1(message);
-  }
-}
-
-//Example for call native by pass parameter as json
-//obj: json
-function example2(obj) {
-  // Call iOS
-  if (window.webkit != undefined) {
-    if (window.webkit.messageHandlers.appInterface != undefined) {
-      window.webkit.messageHandlers.example2.postMessage(obj);
-    }
-  }
-
-  // Call Android
-  if (typeof App !== "undefined") {
-    App.example2(obj);
-  }
-}
-
 
 function openInfomationScene(url) {
   console.log('openInfomationScene: '+url)
