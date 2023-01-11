62ddbb0fb57f3c32f8ee0220f595d78ffd8d0f4a
adisak.j <adisak@ava.fund>
2023-01-11 13:40:06 +0700

    xxx

---
+++ playground/js/mobile.js
@@ -0,0 +1,33 @@
+//##################################################################
+// Example interfaces between your JavaScript code and client-side Android / iOS code
+//##################################################################
+
+function openInfomationScene(url) {
+  console.log('openInfomationScene: '+url)
+  // Call iOS
+  if (window.webkit != undefined) {
+    window.webkit.messageHandlers.openInfomationScene.postMessage(url);
+  } else {
+    console.error("[Error-1]window.webkit is undefined");
+  }
+
+  // Call Android
+  if (typeof App !== "undefined") {
+    App.openInfomationScene(url);
+  }
+}
+
+function setContentHeight(height) {
+  console.log('setContentHeight: '+height)
+  // Call iOS
+  if (window.webkit != undefined) {
+    window.webkit.messageHandlers.setContentHeight.postMessage(height);
+  } else {
+    console.error("[Error-1]window.webkit is undefined");
+  }
+
+  // Call Android
+  if (typeof App !== "undefined") {
+    App.setCustomHeight(obj);
+  }
+}
