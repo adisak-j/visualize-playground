eb93a916c9a6580c552fa1f4e922685860d0d18e
adisak.j <adisak@ava.fund>
2023-05-22 23:40:41 +0700

    m3

--- scripts/merge-main-to-develop.sh
+++ scripts/merge-main-to-develop.sh
@@ -1,9 +1,8 @@
 # Merge branch main to branch develop
 # Usage: ./scripts/merge-main-to-develop.sh
 git checkout develop
-git merge main
+git merge main -m "ci: merge branch main to develop [skip ci]"
 git push origin develop 
-# git push origin develop -m "ci: merge branch main to develop [skip ci]"
 git checkout main   
   
  
